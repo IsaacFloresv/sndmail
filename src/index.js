eb37b0ec12e08ae4dbbc6af6d5ed6127418b1a17
isfloresv <isaacfloresv@gmail.com>
2023-02-11 14:07:11 -0600

    start upload of files of project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import path from "path"
+
+import {enviarEmail} from './routes/sendEmRoute.js'
+
+const app = express()
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.use('/sendemail', enviarEmail)
+
+//app.use(express.static("./src/public"))
+
+
+
+app.listen("3005", ()=> {
+    console.log("Server on port http://127.0.0.1:3005")
+})
